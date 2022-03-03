c9a62a15134290200d4159f3f63871f313af80f3
Coldani3 <p110117069@gmail.com>
2022-03-03 15:42:54 +0000

    Initial commit.

---
+++ app/src/main/java/com/coldani3/dangoniser/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.coldani3.dangoniser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.coldani3.dangoniser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
