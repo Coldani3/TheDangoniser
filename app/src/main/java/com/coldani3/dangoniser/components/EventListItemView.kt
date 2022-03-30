91bfb2a11fa5323de0f48da7b0558a05699d7906
Coldani3 <p110117069@gmail.com>
2022-03-31 00:18:59 +0100

    Implement event list component.

--- app/src/main/java/com/coldani3/dangoniser/components/EventListItemView.kt
+++ app/src/main/java/com/coldani3/dangoniser/components/EventListItemView.kt
@@ -4,19 +4,24 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.RelativeLayout
+import androidx.navigation.findNavController
 import com.coldani3.dangoniser.R
 import com.coldani3.dangoniser.databinding.EventListItemBinding
 
 class EventListItemView : RelativeLayout {
     private lateinit var binding: EventListItemBinding;
     private var eventName: String = "";
+    private var navPath: Int = 0;
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         inflate(context, R.layout.event_list_item, this);
         binding = EventListItemBinding.inflate(LayoutInflater.from(context), this);
+
+        this.setOnClickListener { view -> onClicked(view) }
 
         val attributes: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.EventListItemView, 0, 0);
         binding.eventName.text = attributes.getString(R.styleable.EventListItemView_text);
@@ -25,5 +30,16 @@ class EventListItemView : RelativeLayout {
 
     fun setEventName(name: String) {
         eventName = name;
+    }
+
+    //R.id.action_<etc.>
+    fun setEventNavpath(path: Int) {
+        navPath = path;
+    }
+
+    protected fun onClicked(view: View) {
+        if (navPath != 0) {
+            view.findNavController().navigate(navPath);
+        }
     }
 }
---
+++ app/src/main/java/com/coldani3/dangoniser/components/EventListView.kt
@@ -0,0 +1,27 @@
+package com.coldani3.dangoniser.components
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ScrollView
+import com.coldani3.dangoniser.R
+import com.coldani3.dangoniser.data.CalendarEvent
+import com.coldani3.dangoniser.databinding.EventListBinding
+
+class EventListView : ScrollView {
+    private var binding: EventListBinding;
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        inflate(context, R.layout.event_list_item, this);
+        binding = EventListBinding.inflate(LayoutInflater.from(context), this);
+    }
+
+    public fun addEvent(event: CalendarEvent) {
+        val newItem: EventListItemView = EventListItemView(context);
+        newItem.setEventName(event.eventName);
+        binding.eventsList.addView(newItem);
+    }
+}
