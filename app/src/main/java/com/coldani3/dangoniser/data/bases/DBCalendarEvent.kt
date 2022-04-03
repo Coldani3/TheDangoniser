49ae6fd5da44d7f82f8b954771237f1e0ea1cd65
Coldani3 <p110117069@gmail.com>
2022-04-03 22:27:30 +0100

    Room actually installed so that's neat, fixing a weird error (apparently ' in Kotlin isn't a compile time constant but " is?).

--- app/src/main/java/com/coldani3/dangoniser/data/bases/DBCalendarEvent.kt
+++ app/src/main/java/com/coldani3/dangoniser/data/bases/DBCalendarEvent.kt
@@ -7,9 +7,9 @@ import androidx.room.ColumnInfo
 @Entity
 data class DBCalendarEvent(
     @PrimaryKey val uid: Int,
-    @ColumnInfo(name = 'eventName') val eventName: String,
-    @ColumnInfo(name = 'date') val date: Long,
-    @ColumnInfo(name = 'until') val until: Long,
-    @ColumnInfo(name = 'location') val location: String,
-    @ColumnInfo(name = 'notes') val notes: String,
+    @ColumnInfo(name = "eventName") val eventName: String,
+    @ColumnInfo(name = "date") val date: Long,
+    @ColumnInfo(name = "until") val until: Long,
+    @ColumnInfo(name = "location") val location: String,
+    @ColumnInfo(name = "notes") val notes: String,
 )
